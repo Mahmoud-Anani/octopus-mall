f93f4cd4bcf3e4e7a44f0311e4cc9849c1c84004
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2023-09-02 11:51:02 +0300

    add section services

--- src/components/components-products/ServicesComponent.jsx
+++ src/components/components-products/ServicesComponent.jsx
@@ -1,28 +1,26 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { Box, CardActionArea } from "@mui/material";
+import { Box } from "@mui/material";
 
 function ServicesComponent({ img, title, icon }) {
   return (
     <Box sx={{ maxWidth: 345, width: "100%", marginX: "auto" }}>
-      <CardActionArea className="relative ">
-        <div className="absolute p-3 rounded-full border-1 z-20 bg-[#D1E7FF] right-3 top-[50%]">
+      <div className="relative bg-[#FFF] rounded-lg">
+        <div className="absolute p-3 rounded-full border-1 z-10 bg-[#D1E7FF] right-3 top-[50%]">
           {icon}
         </div>
         <CardMedia
           component="img"
           height="140"
-          className="h-32 opacity-50 "
+          className="h-32 opacity-75 hover:opacity-100 duration-200 rounded-t-lg"
           image={img}
           alt="green iguana"
         />
         <CardContent>
           <p className={`text-[#1C1C1C] text-base font-medium `}>{title}</p>
         </CardContent>
-      </CardActionArea>
+      </div>
     </Box>
   );
 }
